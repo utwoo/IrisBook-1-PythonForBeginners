b44eaab25b40cde96b579660baf2bba81d88e820
zhuxaing1212 <zhuxaing1212@hotmail.com>
2023-11-10 21:43:12 +0800

    热图

--- 11 2D and 3D Visualizations/heatmap_plotly.py
+++ 11 2D and 3D Visualizations/heatmap_plotly.py
@@ -2,20 +2,23 @@ import plotly.express as px
 
 df = px.data.iris()
 
-fig = px.imshow(df.iloc[:, 0:-2],
-                text_auto=False,
+fig = px.imshow(df.iloc[:, 0:-2],   # 通过 iloc 切片选择了数据帧所有行和除了倒数第一、二列之外的所有列
+                text_auto=False,    # 禁用了自动生成文本标签
                 width=600,
                 height=600,
-                x=None,
-                zmin=0,zmax=8,
-                color_continuous_scale='viridis')
+                x=None,             # 横轴上不显示具体的数值
+                zmin=0, zmax=8,      # 颜色映射的范围
+                color_continuous_scale='viridis')   # 颜色映射使用的是viridis色谱
 
-fig.update_layout(yaxis=dict(tickmode='array', tickvals=[]))
+# 更新布局设置
+fig.update_layout(yaxis=dict(tickmode='array', tickvals=[]))  # 隐藏纵轴刻度标签
 
-x_labels=['Sepal length', 'Sepal width',
-          'Petal length', 'Petal width']
-
+# 设置横轴标签
+x_labels = ['Sepal length', 'Sepal width',
+            'Petal length', 'Petal width']
+# 计算列表长度
 x_ticks = list(range(len(x_labels)))
+# 更新fig对象横轴设置
+fig.update_xaxes(tickmode='array', tickvals=x_ticks, ticktext=x_labels)
 
-fig.update_xaxes(tickmode ='array', tickvals = x_ticks, ticktext = x_labels)
 fig.show()
